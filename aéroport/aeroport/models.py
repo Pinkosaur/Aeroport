1412c88ae51ff825861220080b2e6c6126c131de
Pinkosaur <gaetan.thomassin@gmail.com>
2023-06-01 15:33:23 +0200

    aie...

--- aéroport/aeroport/models.py
+++ aéroport/aeroport/models.py
@@ -9,7 +9,6 @@ class Avion(models.Model):
     date_depart = models.DateField(blank=True, null=True)
     nombre_passagers_depart = models.IntegerField(blank=True, null=True)
     nombre_passagers_arrivee = models.IntegerField(blank=True, null=True)
-#    id = models.IntegerField(primary_key=True)
 
     def __str__(self):
         chaine = f"Avion de la compagnie {self.comp} piloté par {self.pilote1} et {self.pilote2}, arrivant le " \
@@ -20,3 +19,7 @@ class Avion(models.Model):
     def dic(self):
         return {"comp":self.comp, "pilote1":self.pilote1, "pilote2":self.pilote2, "date_arrivee":self.date_arrivee,
                 "date_depart":self.date_depart, "nombre_passagers_depart":self.nombre_passagers_depart, "nombre_passagers_arrivee":self.nombre_passagers_arrivee}
+
+
+class Aeroport(models.Model):
+    localisation = models.CharField(max_length=100)
